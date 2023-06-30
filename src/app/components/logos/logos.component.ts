c8231d9587c94f8b21e6e9651140e81870c5abf1
LeandroSBF <121291913+LeandroSBF@users.noreply.github.com>
2023-06-30 09:06:00 -0300

    primer commit estructura front end

---
+++ src/app/components/logos/logos.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logos',
+  templateUrl: './logos.component.html',
+  styleUrls: ['./logos.component.css'],
+})
+export class LogosComponent {}
